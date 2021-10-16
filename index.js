fa13439c43654408c6fc1735a7947b1eaaae05a5
Scott Xu <fgsoap@gmail.com>
2021-10-16 16:16:22 +0800

    add more newspapers

--- index.js
+++ index.js
@@ -6,26 +6,48 @@ const { response } = require('express')
 
 const app = express()
 
+const articles = []
+
+const newspapers = [
+    {
+        name: "thetimes",
+        address: "https://www.thetimes.co.uk/environment/climate-change",
+    },
+    {
+        name: "guardian",
+        address: "https://www.theguardian.com/environment/climate-crisis",
+    },
+    {
+        name: "telegraph",
+        address: "https://www.telegraph.co.uk/climate-change",
+    }
+]
+
+newspapers.forEach(newspaper => {
+    axios.get(newspaper.address)
+        .then(response => {
+            const html = response.data
+            const $ = cheerio.load(html)
+            $('a:contains("climate")', html).each(function () {
+                const title = $(this).text()
+                const url = $(this).attr("href")
+
+                articles.push({
+                    title,
+                    url,
+                    source: newspaper.address
+                })
+
+            })
+        }).catch(err => console.log(err))
+})
+
 app.get('/', (req, res) => {
     res.json('Welcome to my Climate Change News API')
 })
 
 app.get('/news', (req, res) => {
-    const articles = []
-    axios.get('https://www.theguardian.com/environment/climate-crisis')
-        .then((response) => {
-            const html = response.data
-            const $ = cheerio.load(html)
-            $('a:contains("climate")', html).each(function () {
-                const title = $(this).text()
-                const url = $(this).attr('href')
-                articles.push({
-                    title,
-                    url
-                })
-            })
-            res.json(articles)
-        }).catch((err) => console.log(err))
+    res.json(articles)
 })
 
 app.listen(PORT, () => console.log(`server is running on ${PORT}`))
